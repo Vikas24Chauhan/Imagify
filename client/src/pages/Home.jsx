9e7fa5789e4f0d753e34254a4f2a55f32eb037d1
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-02-25 18:15:54 +0530

    steps component added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Steps from "../components/Steps";
 
 function Home() {
   return (
     <div>
       <Header />
+      <Steps />
     </div>
   );
 }
