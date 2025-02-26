301f2d7bae2664dcf5a8be2ba94954a9534e244c
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-02-26 12:53:16 +0530

    Buy Credit page added

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { assets, plans } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 function BuyCredit() {
-  return <div>BuyCredit Page</div>;
+  const { user } = useContext(AppContext);
+
+  return (
+    <div className="min-h-[80vh] text-center pt-14 mb-10">
+      <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">
+        Our Plans
+      </button>
+      <h1 className="text-center text-3xl font-medium mb-6 sm:mb-10">
+        Choose the plan
+      </h1>
+
+      <div className="flex flex-wrap justify-center gap-6 text-left">
+        {plans.map((data, index) => (
+          <div
+            key={index}
+            className="bg-white drop-shadow-sm border rounded-lg py-12 px-8 text-gray-600 hover:scale-105 transition-all duration-500"
+          >
+            <img width={40} src={assets.lock_icon} alt="" />
+            <p className="mt-3 mb-1 font-semibold">{data.id}</p>
+            <p className="text-sm">{data.desc}</p>
+            <p className="mt-6">
+              <span className="text-3xl font-medium">${data.price}</span> /{" "}
+              {data.credits} credits
+            </p>
+            <button className="w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52">
+              {user ? "Purchase" : "Get Started"}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default BuyCredit;
